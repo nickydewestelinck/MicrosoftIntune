303f870e255b090b6f81f886030e38698c096fad
Nicky De Westelinck <nicky.dewestelinck@outlook.com>
2024-02-08 14:32:54 +0100

    Delete CasesFromTheField /TeamsBackgrounds directory

--- CasesFromTheField /TeamsBackgrounds/Detection/detectionupload.ps1
+++
@@ -1,4 +0,0 @@
-# This script will be used to detect if the upload was succesfull
-if (Test-Path "$env:APPDATA\Microsoft\Teams\Backgrounds\Uploads\*.jpg") {
-    Write-Host "The Teams Background Files are detected"
-}
